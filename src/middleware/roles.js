dfd8dedde8a570be1152718ab38088ff1398fd7f
Miguel Abache <miiguel.abache@gmail.com>
2023-08-20 20:34:18 -0400

    cambio

--- src/middleware/roles.js
+++ src/middleware/roles.js
@@ -6,7 +6,7 @@ const checkRole = (req, res, next) => {
     // Ya como habiamos inyectado el usuario a req , lo podemos extraer de alli
     const { user } = req;
     // Sacamos el role al que pertenece el usuario
-    const rolesByUser = user.roe;
+    const rolesByUser = user.role;
   } catch (e) {
     handleHttpError(res, "ERROR_PERMISSION", 403);
   }
